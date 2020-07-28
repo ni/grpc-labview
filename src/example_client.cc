43b0cb85926220bc2f06dd1390a6f76df9b10da3
Christopher Cifra <christopher.cifra@ni.com>
2020-07-28 06:58:12 -0500

    adding certificate support to the c++ example client

--- src/example_client.cc
+++ src/example_client.cc
@@ -4,6 +4,10 @@
 #include <grpcpp/health_check_service_interface.h>
 #include <grpcpp/ext/proto_server_reflection_plugin.h>
 #include <query_server.grpc.pb.h>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <filesystem>
 
 //---------------------------------------------------------------------
 //---------------------------------------------------------------------
@@ -124,10 +128,70 @@ string GetServerAddress(int argc, char** argv)
 
 //---------------------------------------------------------------------
 //---------------------------------------------------------------------
+string GetCertPath(int argc, char** argv)
+{
+    string cert_str;
+    string arg_str("--cert");
+    if (argc > 2)
+    {
+        string arg_val = argv[2];
+        size_t start_pos = arg_val.find(arg_str);
+        if (start_pos != string::npos)
+        {
+            start_pos += arg_str.size();
+            if (arg_val[start_pos] == '=')
+            {
+                cert_str = arg_val.substr(start_pos + 1);
+            }
+            else
+            {
+                cout << "The only correct argument syntax is --cert=" << endl;
+                return 0;
+            }
+        }
+        else
+        {
+            cout << "The only acceptable argument is --cert=" << endl;
+            return 0;
+        }
+    }
+    return cert_str;
+}
+
+std::string read_keycert( const std::string& filename)
+{	
+	std::string data;
+	std::ifstream file(filename.c_str(), std::ios::in);
+	if (file.is_open())
+	{
+		std::stringstream ss;
+		ss << file.rdbuf();
+		file.close();
+		data = ss.str();
+	}
+	return data;
+}
+
+//---------------------------------------------------------------------
+//---------------------------------------------------------------------
 int main(int argc, char **argv)
 {
     auto target_str = GetServerAddress(argc, argv);
-    QueryClient client(grpc::CreateChannel(target_str, grpc::InsecureChannelCredentials()));
+    auto certificatePath = GetCertPath(argc, argv);
+
+    shared_ptr<grpc::ChannelCredentials> creds;
+    if (!certificatePath.empty())
+    {
+        std::string cacert = read_keycert(certificatePath);
+        grpc::SslCredentialsOptions ssl_opts;
+        ssl_opts.pem_root_certs=cacert;
+        creds = grpc::SslCredentials(ssl_opts);
+    }
+    else
+    {
+        creds = grpc::InsecureChannelCredentials();
+    }
+    QueryClient client(grpc::CreateChannel(target_str, creds));
 
     auto result = client.Query("Uptime");
     cout << "Server uptime: " << result << endl;
