f1e2238bbe3fcd1bdb82c2c8d44ddd7d61596943
Christopher Cifra <christopher.cifra@ni.com>
2021-02-14 13:51:35 -0600

    Cleanup the query server example to remove test code Added a test project which is based on the query server example Cleanup cmake file

--- src/test_client.cc
+++ src/test_client.cc
@@ -3,7 +3,7 @@
 #include <grpcpp/grpcpp.h>
 #include <grpcpp/health_check_service_interface.h>
 #include <grpcpp/ext/proto_server_reflection_plugin.h>
-#include <query_server.grpc.pb.h>
+#include <data_marshal.grpc.pb.h>
 #include <sstream>
 #include <fstream>
 #include <iostream>
@@ -175,6 +175,109 @@ std::string read_keycert( const std::string& filename)
 
 //---------------------------------------------------------------------
 //---------------------------------------------------------------------
+void DoDataTypeTest(QueryClient& client)
+{    
+    ClientContext ctx;
+    TestDataTypesParameters request;
+    request.set_root("Root String");
+    request.mutable_repeatedstring()->Add("String 1");
+    request.mutable_repeatedstring()->Add("String 2");
+    request.mutable_repeatedstring()->Add("String 3");
+
+    request.mutable_repeatedintvalue()->Add(1);
+    request.mutable_repeatedintvalue()->Add(2);
+    request.mutable_repeatedintvalue()->Add(3);
+    request.mutable_repeatedintvalue()->Add(4);
+    request.mutable_repeatedintvalue()->Add(5);
+    request.mutable_repeatedintvalue()->Add(6);
+
+    request.set_rootint32(42);
+    request.set_rootdouble(42.42);
+    request.set_rootbool(true);
+    request.set_rootfloat(37.56f);
+
+    request.mutable_repeateddoublevalue()->Add(1.1f);
+    request.mutable_repeateddoublevalue()->Add(2.2f);
+    request.mutable_repeateddoublevalue()->Add(3.3f);
+    request.mutable_repeateddoublevalue()->Add(4.4f);
+    request.mutable_repeateddoublevalue()->Add(5.5f);
+    request.mutable_repeateddoublevalue()->Add(6.6f);
+
+    request.mutable_repeatedboolvalue()->Add(true);
+    request.mutable_repeatedboolvalue()->Add(true);
+    request.mutable_repeatedboolvalue()->Add(false);
+    request.mutable_repeatedboolvalue()->Add(true);
+    request.mutable_repeatedboolvalue()->Add(true);
+    request.mutable_repeatedboolvalue()->Add(false);
+
+    request.mutable_repeatedfloatvalue()->Add(1.23f);
+    request.mutable_repeatedfloatvalue()->Add(2.23f);
+    request.mutable_repeatedfloatvalue()->Add(3.23f);
+    request.mutable_repeatedfloatvalue()->Add(4.23f);
+    request.mutable_repeatedfloatvalue()->Add(5.23f);
+    request.mutable_repeatedfloatvalue()->Add(6.23f);
+
+    request.mutable_nested()->set_boolvalaue(true);
+    request.mutable_nested()->set_doublevalue(12.12);
+    request.mutable_nested()->set_intvalue(4242);
+    request.mutable_nested()->set_floatvalue(15.14f);
+
+    auto nested = request.mutable_repeatednested()->Add();
+    nested->set_boolvalaue(true);
+    nested->set_floatvalue(1.1f);
+    nested = request.mutable_repeatednested()->Add();
+    nested->set_boolvalaue(false);
+    nested->set_floatvalue(2.2f);
+    nested = request.mutable_repeatednested()->Add();
+    nested->set_boolvalaue(true);
+    nested->set_floatvalue(3.3f);
+
+    TestDataTypesParameters response;
+    auto result = client.m_Stub->TestDataTypes(&ctx, request, &response);
+
+    cout << response.root() << endl;
+    for (auto i: response.repeatedstring())
+    {
+        cout << i << endl;
+    }
+    for (int x = 0; x < response.mutable_repeatedintvalue()->size(); ++x)
+    {
+        cout << response.mutable_repeatedintvalue()->data()[x] << endl;
+    }
+
+    cout << response.rootint32() << endl;
+    cout << response.rootdouble() << endl;
+    cout << response.rootbool() << endl;
+    cout << response.rootfloat() << endl;
+
+    for (int x = 0; x < response.mutable_repeateddoublevalue()->size(); ++x)
+    {
+        cout << response.mutable_repeateddoublevalue()->data()[x] << endl;
+    }
+
+    for (int x = 0; x < response.mutable_repeatedfloatvalue()->size(); ++x)
+    {
+        cout << response.mutable_repeatedfloatvalue()->data()[x] << endl;
+    }
+
+    for (int x = 0; x < response.mutable_repeatedboolvalue()->size(); ++x)
+    {
+        cout << response.mutable_repeatedboolvalue()->data()[x] << endl;
+    }
+
+    cout << response.nested().boolvalaue() << endl;
+    cout << response.nested().doublevalue() << endl;
+    cout << response.nested().intvalue() << endl;
+    cout << response.nested().floatvalue() << endl;
+
+    for (int x = 0; x < response.repeatednested().size(); ++x)
+    {
+        cout << response.repeatednested()[x].boolvalaue() << ", " << response.repeatednested()[x].floatvalue() << endl;
+    }
+}
+
+//---------------------------------------------------------------------
+//---------------------------------------------------------------------
 int main(int argc, char **argv)
 {
     auto target_str = GetServerAddress(argc, argv);
@@ -194,6 +297,8 @@ int main(int argc, char **argv)
     }
     auto channel = grpc::CreateChannel(target_str, creds);
     QueryClient client(channel);
+
+    DoDataTypeTest(client);
 
     auto result = client.Query("Uptime");
     cout << "Server uptime: " << result << endl;
