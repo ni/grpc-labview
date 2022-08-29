8a47c9652b4f4d744a4dfcbda091928fbc110bb0
Chad Allee <chad.allee@ni.com>
2022-08-29 14:01:23 -0500

    code review: don't use .hpp

--- src/token_manager.h
+++ src/token_manager.h
@@ -6,6 +6,10 @@
 
 namespace grpc_labview
 {
+    //---------------------------------------------------------------------
+    // Definition
+    //---------------------------------------------------------------------
+
     /// A token manager to track & manage the lifetime of pointers
     /// This class serves two purposes:
     /// 1. Ensure the validity of pointers -- This class knows which tokens are valid, so if
@@ -43,6 +47,67 @@ namespace grpc_labview
         std::map<token_type, std::shared_ptr<T>> _pointerForToken;
         std::mutex _mutex;
     };
-}
 
-#include "token_manager.hpp"
+    //---------------------------------------------------------------------
+    // Implementation
+    //---------------------------------------------------------------------
+
+    template <typename T>
+    TokenManager<T>::TokenManager() :
+        _pointerForToken(),
+        _mutex()
+    {
+    }
+
+    template <typename T>
+    template <typename TDerivedType>
+    std::shared_ptr<TDerivedType> TokenManager<T>::GetPtrForToken(TokenManager::token_type token, int32_t* status)
+    {
+        std::lock_guard<std::mutex> lock(_mutex);
+        auto ptr = _pointerForToken.find(token);
+        if (ptr == _pointerForToken.end())
+        {
+            std::cerr << "ERROR: NO POINTER EXISTS FOR THIS TOKEN" << std::endl;
+            if (status != nullptr)
+            {
+                *status = -1;
+            }
+            return std::shared_ptr<TDerivedType>(nullptr);
+        }
+
+        std::shared_ptr<TDerivedType> derivedPtr = std::dynamic_pointer_cast<TDerivedType>(ptr->second);
+        if (!derivedPtr)
+        {
+            std::cerr << "ERROR: CASTING POINTER TO MORE SPECIFIC TYPE FAILED" << std::endl;
+            if (status != nullptr)
+            {
+                *status = -1;
+            }
+            return std::shared_ptr<TDerivedType>(nullptr);
+        }
+
+        return derivedPtr;
+    }
+
+    template <typename T>
+    typename TokenManager<T>::token_type TokenManager<T>::CreateTokenForPtr(T* ptr)
+    {
+        std::lock_guard<std::mutex> lock(_mutex);
+        // NOTE: With this implementation of token generation (just reinterpreting the pointer to an int),
+        // we could just use a std::set instead of a std::map since the key & value are basically identical.
+        // But I like this approach so that we have the option to obfuscate the token we return to the user.
+        auto tokenForPtr = reinterpret_cast<typename TokenManager<T>::token_type>(ptr);
+        if (_pointerForToken.find(tokenForPtr) == _pointerForToken.end())
+        {
+            _pointerForToken.insert({ tokenForPtr, std::shared_ptr<T>(ptr) });
+        }
+        return tokenForPtr;
+    }
+
+    template <typename T>
+    bool TokenManager<T>::DestroyTokenForPtr(TokenManager::token_type token)
+    {
+        std::lock_guard<std::mutex> lock(_mutex);
+        return _pointerForToken.erase(token) > 0;
+    }
+}
