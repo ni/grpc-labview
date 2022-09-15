b1f51685eca82e7c4bc2b81a78df2b3c6a27c676
Shivaprasad J. Basavaraj <shivaprasad.basavaraj@ni.com>
2022-09-15 14:15:07 +0530

    Minor cleanup

--- src/grpc_load.cc
+++ src/grpc_load.cc
@@ -1,22 +1,26 @@
 #ifndef _WIN32
 #include <dlfcn.h>
-typedef void* LibHandle;
 #else
 #include <wtypes.h>
-typedef HMODULE LibHandle;
 #endif
-
-LibHandle LockgRPCLibraryIntoProcessMem()
-{
-#if _WIN32
-    auto dllHandle = LoadLibrary("labview_grpc_server.dll");
-#else
-    auto dllHandle = dlopen("liblabview_grpc_server.so", RTLD_LAZY);
-#endif
-    return dllHandle;
-}
 
 namespace grpc_labview
 {
+#ifndef _WIN32
+    typedef void* LibHandle;
+#else
+    typedef HMODULE LibHandle;
+#endif
+
+    LibHandle LockgRPCLibraryIntoProcessMem()
+    {
+#if _WIN32
+        auto dllHandle = LoadLibrary("labview_grpc_server.dll");
+#else
+        auto dllHandle = dlopen("liblabview_grpc_server.so", RTLD_LAZY);
+#endif
+        return dllHandle;
+    }
+
     LibHandle gSelfLibHandle = LockgRPCLibraryIntoProcessMem();
 }
