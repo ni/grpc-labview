c746317fc4da53667e7e0471c4f76a605790eed7
Ayush Kumar Baheti <ayush.kumar.baheti@ni.com>
2022-09-23 16:44:57 +0530

    implemented review comments

--- build-it/stage_artifacts.py
+++ build-it/stage_artifacts.py
@@ -16,9 +16,9 @@ _logger.addHandler(handler)
 
 def main():
     args = parse_args()
-    _logger.debug(f"Artifacts path is {os.listdir(args.downloaded_path)}")
+    _logger.debug(f"Downloaded artifacts are at {os.listdir(args.downloaded_path)}")
+    # args.downloaded_path contains artifact folders(each artifact is downloaded as a folder of its own)
     artifact_folders = [os.path.join(args.downloaded_path, x) for x in os.listdir(args.downloaded_path)]
-    _logger.debug(f"the downloaded artifacts are at {args.downloaded_path}")
     stage_artifacts_for_export(artifact_folders, args.staging_path)
 
 def parse_args():
@@ -38,20 +38,20 @@ def parse_args():
     _logger.debug(f"The parameters for the script are: {parser.parse_args()}")
     return parser.parse_args()
 
-def stage_artifacts_for_export(artifact_folders, working_directory):
-    tar_staging_directory = prepare_directory(working_directory, "Tar_Staging_Directory")
-    for artifact in artifact_folders:
-        _logger.debug(f"copy {artifact} to {tar_staging_directory}")
-        copy_tar_artifacts(artifact, tar_staging_directory)
-    stage_artifacts(working_directory, tar_staging_directory)
+def stage_artifacts_for_export(artifact_folders, input_staging_directory):
+    tar_staging_directory = prepare_directory(parent_directory=input_staging_directory, sub_directory="Tar_Staging_Directory")
+    for artifact_folder in artifact_folders:
+        _logger.debug(f"copy contents of {artifact_folder} to {tar_staging_directory}")
+        copy_tar_artifacts(artifact_folder, tar_staging_directory)
+    stage_artifacts(input_staging_directory, tar_staging_directory)
 
-def copy_tar_artifacts(artifact_path, tar_staging_directory):
-    _logger.debug(f"{artifact_path}")
-    tar_artifact = os.listdir(artifact_path)
-    shutil.copy(Path(artifact_path)/tar_artifact[0], tar_staging_directory)
+# add a comment this assumes that each artifact folder has only one tar.gz
+def copy_tar_artifacts(artifact_folder, tar_staging_directory):
+    tar_artifact = os.listdir(artifact_folder)
+    shutil.copy(Path(artifact_folder)/tar_artifact[0], tar_staging_directory)
 
-def prepare_directory(working_directory, sub_directory):
-    new_directory = Path(working_directory) / sub_directory
+def prepare_directory(parent_directory, sub_directory):
+    new_directory = Path(parent_directory) / sub_directory
     if not os.path.exists(new_directory):
         os.makedirs(new_directory, exist_ok=True)
     return new_directory
@@ -62,10 +62,10 @@ def stage_artifacts(working_directory, tar_staging_directory):
     for tar_artifact in tar_artifacts:
         extract_tarfile(tar_artifact, tar_staging_directory, top_level_dll_directory)
 
-def extract_tarfile(tar_file_name, staging_directory, top_level_dll_directory):
+def extract_tarfile(tar_file_name, tar_staging_directory, top_level_dll_directory):
     _logger.debug(f"Creating folder Heirarchy for Server Dlls...")
     _logger.debug(f"extracing tar file {tar_file_name}...")
-    with tarfile.open(Path(staging_directory) / tar_file_name) as tar_file:
+    with tarfile.open(Path(tar_staging_directory) / tar_file_name) as tar_file:
         for artifact in tar_file.getmembers():
             if(tar_file_name.find('x64') != -1):
                 bitness_type = 'Win64'
@@ -76,7 +76,7 @@ def extract_tarfile(tar_file_name, staging_directory, top_level_dll_directory):
             elif(tar_file_name.find('rt') != -1):
                 bitness_type = 'LinuxRT'
             extract_and_stage_artifact(artifact, tar_file, bitness_type, top_level_dll_directory)
-        _logger.debug(f"extracted {tar_file.getnames()} to {staging_directory}")
+        _logger.debug(f"extracted {tar_file.getnames()} to {tar_staging_directory}")
 
 def extract_and_stage_artifact(artifact, tar_file, bitness_type, top_level_dll_directory):
     server_folder = prepare_directory(top_level_dll_directory, sub_directory="LabVIEW gRPC Server")
