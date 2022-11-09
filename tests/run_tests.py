c077b1217aa750b9030e38d864f8615190139cab
Ayush Kumar Baheti <ayush.kumar.baheti@ni.com>
2022-11-09 15:17:18 +0530

    Run tests using lst files

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,6 +1,4 @@
 
-import glob
-import json
 import logging
 import os
 import subprocess
@@ -21,29 +19,18 @@ def main():
 def traverse_tests():
     test_directory = os.path.abspath(os.path.dirname(__file__))
     _logger.debug(f"Finding tests in {test_directory}...")
-    allTestJSONs = glob.glob(test_directory + '/*.json', recursive=True)
-    for testJSON in allTestJSONs:
-        WrapperVI, testVI =  readTestVILocationForJson(testJSON)
-        _logger.debug(f"Running {testVI}")
-        runTest(WrapperVI, testVI)
+    WrapperVI = os.path.join(test_directory, "LV_ATS\\gRPC_ATS.vi")
+    allTestJSONs = os.path.join(test_directory , "Tests.lst")
+    with open(allTestJSONs, 'r') as file:
+        alltests = file.readlines()
+        for test in alltests:
+            runTest(WrapperVI, test)
 
 def runTest(WrapperVI, testVI):
     testResult = subprocess.run(["LabVIEWCLI", "-OperationName", "RunVI", "-VIPath", os.path.normpath(WrapperVI), testVI], capture_output= True)
     if(testResult.returncode == 0):
-        _logger.debug(f"[PASSED] {testVI} ")
+        _logger.debug(f"[PASSED] {testVI}")
     else:
-        _logger.debug(f"[FAILED] {testVI} has failed with {testResult.stdout.decode()}")
-
-def readTestVILocationForJson(testJSON):
-    #read json file
-    with open(testJSON, 'r') as myfile:
-        data = myfile.read()    
-    jsonObj = json.loads(data)
-    _logger.debug(f"JOSN object = {jsonObj}")
-    WrapperVI = str(jsonObj[0]["VI Specs"][0]["VI Path"])
-    _logger.debug(f"Wrapper VI: {WrapperVI}")
-    TestVI = str(jsonObj[0]["VI Specs"][0]["Test Json"]["VI path"])
-    _logger.debug(f"Test VI {TestVI}")
-    return WrapperVI, TestVI
+        _logger.error(f"[FAILED] {testVI} has failed with {testResult.stdout.decode()}", exc_info=1)
 
 main()
