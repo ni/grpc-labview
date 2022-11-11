20cdbce610bfc0bcb2cfa7806ea980f8bf0cc432
Ayush Kumar Baheti <ayush.kumar.baheti@ni.com>
2022-11-11 13:08:40 +0530

    Updated wrappers

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -21,16 +21,19 @@ def traverse_tests():
     _logger.debug(f"Finding tests in {test_directory}...")
     WrapperVI = os.path.join(test_directory, "LV_ATS\\gRPC_ATS.vi")
     allTestJSONs = os.path.join(test_directory , "Tests.lst")
+    failed_flag = ""
     with open(allTestJSONs, 'r') as file:
         alltests = file.readlines()
         for test in alltests:
-            runTest(WrapperVI, test)
+            failed_flag += runTest(WrapperVI, test)
+        if (failed_flag != ""):
+            raise Exception(failed_flag)
 
 def runTest(WrapperVI, testVI):
     testResult = subprocess.run(["LabVIEWCLI", "-OperationName", "RunVI", "-VIPath", os.path.normpath(WrapperVI), testVI], capture_output= True)
     if(testResult.returncode == 0):
         _logger.debug(f"[PASSED] {testVI}")
     else:
-        _logger.error(f"[FAILED] {testVI} has failed with {testResult.stdout.decode()}", exc_info=1)
-
+        _logger.exception(f"[FAILED] {testVI} has failed with {testResult.stdout.decode()} {testResult.stderr.decode()}")
+        return (testResult.stdout.decode() + "\n" + testResult.stderr.decode() + "\n")
 main()
