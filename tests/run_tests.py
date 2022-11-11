c6366c3575f9dff71d0fd3375bbf5d8ae188088f
Ayush Kumar Baheti <ayush.kumar.baheti@ni.com>
2022-11-11 20:17:01 +0530

    fixed broken path

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -4,7 +4,6 @@ import os
 import subprocess
 import sys
 
-
 _logger = logging.getLogger(__name__)
 _logger.setLevel(logging.DEBUG)
 
@@ -14,25 +13,28 @@ _logger.addHandler(handler)
 
 def main():
     _logger.debug(f"Looking for tests")
-    traverse_tests()
+    run_all_tests()
 
-def traverse_tests():
+def run_all_tests():
     test_directory = os.path.abspath(os.path.dirname(__file__))
     _logger.debug(f"Finding tests in {test_directory}...")
-    WrapperVI = os.path.join(test_directory, "LV_ATS\\gRPC_ATS.vi")
-    allTestJSONs = os.path.join(test_directory , "Tests.lst")
-    failed_flag = ""
-    with open(allTestJSONs, 'r') as file:
-        alltests = file.readlines()
-        for test in alltests:
-            failed_flag += runTest(WrapperVI, test)
-        if (failed_flag != ""):
-            raise Exception(failed_flag)
+    test_runner_vi = os.path.join(test_directory, "LV_ATS\\gRPC_ATS.vi")
+    test_file = os.path.join(test_directory , "Tests.lst")
+    failed_test_results = ""
+    with open(test_file, 'r') as file:
+        all_tests = file.readlines()
+        for test in all_tests:
+            run_result = run_test(test_runner_vi, os.path.join(test_directory, test))
+            if(run_result != None):
+                failed_test_results += run_result
+        if (failed_test_results != ""):
+            raise Exception(failed_test_results)
 
-def runTest(WrapperVI, testVI):
+def run_test(WrapperVI, testVI):
     testResult = subprocess.run(["LabVIEWCLI", "-OperationName", "RunVI", "-VIPath", os.path.normpath(WrapperVI), testVI], capture_output= True)
     if(testResult.returncode == 0):
         _logger.debug(f"[PASSED] {testVI}")
+        return
     else:
         _logger.exception(f"[FAILED] {testVI} has failed with {testResult.stdout.decode()} {testResult.stderr.decode()}")
         return (testResult.stdout.decode() + "\n" + testResult.stderr.decode() + "\n")
