469ad049e1d716ab23e6ba1535a59acdcfb1ed06
yash-ni <131851960+yash-ni@users.noreply.github.com>
2023-09-01 09:40:24 +0530

    Added Streaming Tests to Testing Suite (#303)
    * added routeguide test to NEW_ATS
    
    * modified existing tests
    
    * fixed bug in RunPythonClient.bat
    
    * Updated NEW_ATS's README.md
    
    * Fixed stash_server_impl and enabled gitub workflows to download NEW_ATS logs
    
    * Added delay in RecordRoute.json
    
    * Reverted routeguide example
    
    * Updated README

--- tests/New_ATS/Tests/simple-oneof/Python_client/GetFeature_client.py
+++ tests/New_ATS/Tests/simple-oneof/Python_client/GetFeature_client.py
@@ -1,6 +1,6 @@
 import grpc
-import enum_oneof_pb2
-import enum_oneof_pb2_grpc
+import simple_oneof_pb2
+import simple_oneof_pb2_grpc
 import json
 import pytest
 import os
@@ -8,17 +8,18 @@ import os
 def get_GetFeature_output(test_input):
     req_latitude = int(test_input.get('req_latitude'))
     req_name = test_input['req_oneof'].get('req_name')
-    req_color = test_input['req_oneof'].get('req_color')
+    req_num = test_input['req_oneof'].get('req_num')
     with grpc.insecure_channel('localhost:50051') as channel:
-        stub = enum_oneof_pb2_grpc.GreeterServiceStub(channel)
-        request = enum_oneof_pb2.req(req_latitude=req_latitude, req_name=req_name, req_color=req_color)
+        stub = simple_oneof_pb2_grpc.GreeterServiceStub(channel)
+        request = simple_oneof_pb2.req(req_latitude=req_latitude, req_name=req_name, req_num=req_num)
         response = stub.GetFeature(request)
     response_dict = {}
     response_dict['res_latitude'] = response.res_latitude
     response_dict['res_oneof'] = {}  
-    response_dict['res_oneof']['res_color'] = enum_oneof_pb2.Color.Name(response.res_color) if response.HasField('res_color') else None
-    response_dict['res_oneof']['res_name'] = response.res_name if response.HasField('res_name') else None
-
+    if response.HasField('res_num'):
+        response_dict['res_oneof']['res_num'] = response.res_num
+    if response.HasField('res_name'):
+        response_dict['res_oneof']['res_name'] = response.res_name
     return(response_dict)
 
 def read_json(filepath):
@@ -26,7 +27,7 @@ def read_json(filepath):
         test_data = json.load(file)
     return test_data
 
-GetFeature_json_file_path = f'{os.path.dirname(os.path.abspath(__file__))}/testcases/GetFeature.json'
+GetFeature_json_file_path = f'{os.path.dirname(os.path.dirname(os.path.abspath(__file__)))}/testcases/GetFeature.json'
 
 @pytest.mark.parametrize('testcase', read_json(GetFeature_json_file_path))
 def test_SayHello(testcase):
