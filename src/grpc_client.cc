ab0ea6af3782bfcd89c647639ba006abc2d85093
Yash Chauhan <yash.chauhan@ni.com>
2023-11-10 06:01:17 +0530

    Made changes for all streaming cases

--- src/grpc_client.cc
+++ src/grpc_client.cc
@@ -564,11 +564,11 @@ LIBRARY_EXPORT int32_t ClientBeginBidiStreamingCall(
 
 //---------------------------------------------------------------------
 //---------------------------------------------------------------------
-LIBRARY_EXPORT int32_t ClientBeginReadFromStream(grpc_labview::gRPCid* callId, grpc_labview::MagicCookie* occurrencePtr)
+LIBRARY_EXPORT int32_t ClientBeginReadFromStream(grpc_labview::gRPCid* callId, grpc_labview::MagicCookie* occurrencePtr, int8_t* responseCluster)
 {
     auto reader = callId->CastTo<grpc_labview::StreamReader>();
     auto call = callId->CastTo<grpc_labview::ClientCall>();
-
+    call->_response->setLVClusterHandle(reinterpret_cast<const char*>(responseCluster));
     auto occurrence = *occurrencePtr;
 
     if (!reader || !call)
@@ -605,14 +605,14 @@ LIBRARY_EXPORT int32_t ClientCompleteReadFromStream(grpc_labview::gRPCid* callId
     *success = reader->_readFuture.get();
     if (*success)
     {
-        try
+        /*try
         {
             grpc_labview::ClusterDataCopier::CopyToCluster(*call->_response.get(), responseCluster);
         }
         catch (grpc_labview::InvalidEnumValueException& e)
         {
             return e.code;
-        }
+        }*/
     }
     return 0;
 }
@@ -672,7 +672,7 @@ LIBRARY_EXPORT int32_t FinishClientCompleteClientStreamingCall(
     int32_t result = 0;
     if (call->_status.ok())
     {
-        try
+        /*try
         {
             grpc_labview::ClusterDataCopier::CopyToCluster(*call->_response.get(), responseCluster);
         }
@@ -683,7 +683,7 @@ LIBRARY_EXPORT int32_t FinishClientCompleteClientStreamingCall(
             {
                 grpc_labview::SetLVString(errorMessage, e.what());
             }
-        }
+        }*/
     }
     else
     {
@@ -706,9 +706,10 @@ LIBRARY_EXPORT int32_t FinishClientCompleteClientStreamingCall(
 
 //---------------------------------------------------------------------
 //---------------------------------------------------------------------
-LIBRARY_EXPORT int32_t ClientCompleteClientStreamingCall(grpc_labview::gRPCid* callId, grpc_labview::MagicCookie* occurrencePtr)
+LIBRARY_EXPORT int32_t ClientCompleteClientStreamingCall(grpc_labview::gRPCid* callId, grpc_labview::MagicCookie* occurrencePtr, int8_t* responseCluster)
 {
     auto call = callId->CastTo<grpc_labview::ClientCall>();
+    call->_response->setLVClusterHandle(reinterpret_cast<const char*>(responseCluster));
     if (!call)
     {
         return -1;
