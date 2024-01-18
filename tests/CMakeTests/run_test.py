9619598929448b135a79fcebe460bab34cc8b108
Yash Chauhan <yash.chauhan@ni.com>
2024-01-19 02:11:54 +0530

    Reverted os specific prints

--- tests/CMakeTests/run_test.py
+++ tests/CMakeTests/run_test.py
@@ -80,19 +80,16 @@ def run_tests():
             print(result_exported_functions_addition_output ,'\n')
             log_file.write(result_exported_functions_addition_output)
         else:
-            log_file.write("You have not added these function in the 'Exported Functions Cmake Test: " + "\n\n" + result_exported_functions_addition_output + "Please add these function(s) in tests/CMakeTests/testcases/ExportedFunctionList.json")
-            warning_output = "::warning::" + "You have not added these function in the 'Exported Functions Cmake Test: " + " ".join(result_exported_functions_addition_output.split('\n')) + ". Please add these function(s) in tests/CMakeTests/testcases/ExportedFunctionList.json"
-            if os.name == 'nt':
-                subprocess.run(f"echo {warning_output}", shell=True)
-            else:
-                print(warning_output)
+            log_file.write("You have not added these function in the 'Exported Functions Cmake Test': " + "\n\n" + result_exported_functions_addition_output + "Please add these function(s) in tests/CMakeTests/testcases/ExportedFunctionList.json")
+            warning_output = "\:\:warning\:\:" + "You have not added these function in the 'Exported Functions Cmake Test': " + " ".join(result_exported_functions_addition_output.split('\n')) + ". Please add these function(s) in tests/CMakeTests/testcases/ExportedFunctionList.json"
+            print(warning_output)
     if result_exported_functions_addition.returncode != 0:
         result_exported_functions_addition = subprocess.run(["python", str(exported_functions_addition_test_path)])
     if result_exported_functions_addition.returncode != 0:
         print(f"Function structural tests failed with exit code {result_exported_functions_addition.returncode}. Exiting.")
         sys.exit(result_exported_functions_addition.returncode)
     
-    print("All structural tests passed.")
+    print("\nAll structural tests passed.")
 
 if __name__ == "__main__":
     create_virtual_environment()
