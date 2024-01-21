cb6109a6aab92c892fd71bc6030e3d92f4969047
Yash Chauhan <yash.chauhan@ni.com>
2024-01-21 20:23:34 +0530

    Added support for function overloading

--- tests/CMakeTests/tests/exported_functions_addition_test.py
+++ tests/CMakeTests/tests/exported_functions_addition_test.py
@@ -16,9 +16,12 @@ old_function_map = get_exported_functions.getFunctionMap(old_function_list)
 if new_function_list["size"] != old_function_list["size"]:
     func_name = ""
     cnt = 1
-    for func in new_function_map.keys():
-        if old_function_map.get(func) == None:
-           func_name += str(cnt) + ") " + func + '\n'
-           cnt += 1
+    for function_name in new_function_map.keys():
+        new_function_list = new_function_map.get(function_name)
+        old_function_list = old_function_map.get(function_name)
+        for function_signature in new_function_list:
+            if (old_function_list == None) or (function_signature not in old_function_list):
+                func_name += str(cnt) + ") " + function_signature['function_name'] + '\n'
+                cnt += 1
 
     print(f"{func_name}")
