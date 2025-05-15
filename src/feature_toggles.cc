3aee67ae5c4d2c61ff930df06eb1b9a4375fefec
Jeff Plotzke <jplotzke@konrad-technologies.com>
2025-05-15 08:22:28 -0400

    Updated to break apart functions into better logical groups; Updated ReadFeatureFile to take in no/relative/absolute paths

--- src/feature_toggles.cc
+++ src/feature_toggles.cc
@@ -1,28 +1,70 @@
-#include <feature_toggles.h>
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <map>
 #include <algorithm>
+#include <filesystem>
 
+#include "feature_toggles.h"
 #include "path_support.h"
 
 
 namespace grpc_labview {
+
+    // Function to reload all features from file.
+    // If filePath is empty, the default feature configuration file will be used
+    // If filePath contains a path, that specific configuration file will be used
+    //   This filePath can contain either a filename (which is assumed to be in the
+    //   same directory as this shared library) or an absolute path.
+    // 
+    // Note that all features will be re-initialized using the new file.
+    void FeatureConfig::ReloadFeaturesFromFile(const std::string& filePath) {
+        // Clear all features from the map to re-initialize
+        featureFlags.clear();
+
+        // Apply the default feature configuration and then read from the passed in feature config file
+        ApplyDefaultFeatures();
+        ReadFeatureFile(filePath);
+
+        //TODO: remove this post fixing LVMessageEfficient to let enable feature. See issue: #433
+        if (featureFlags.find("data_EfficientMessageCopy") != featureFlags.end())
+            featureFlags["data_EfficientMessageCopy"] = false;
+    }
+
+    // Function to check if a feature is enabled
+    bool FeatureConfig::IsFeatureEnabled(const std::string& featureName) const {
+        auto it = featureFlags.find(featureName);
+        return (it != featureFlags.end()) ? it->second : false;
+    }
+
     // Function to read feature configurations from an INI file
-    void FeatureConfig::ReadConfigFromFile(const std::string& filePath) {
-        std::string configFilePath = filePath;
+    void FeatureConfig::ReadFeatureFile(const std::string& filePath) {
+        std::filesystem::path configFilePath = filePath;
         
         if (configFilePath.empty()) {
             // If no filepath passed into this function, use the default
             // configuration file path.
             configFilePath = GetDefaultConfigPath();
         }
+        else if (configFilePath.is_relative()) {
+            // If a relative path (ie, just a filename) was passed in, 
+            // assume that the is in the current directory
+            configFilePath = GetFolderContainingDLL() / configFilePath;
+        }
 
-        std::ifstream configFile(configFilePath);
+        // This flag is used to indicate that the default/given feature file was found/used.
+        // Reset this flag to set to true after the file is successfully opened
+        featureFlags["featureFileFound"] = false;
+
+        std::ifstream configFile(configFilePath.string());
         if (!configFile.is_open()) {
             return;
         }
+
+        // Set this feature to true once the file is opened.  Note that this feature flag
+        // does not indicate if the file was successfully parsed; only that it was found
+        // and opened.
+        featureFlags["featureFileFound"] = true;
 
         std::string line;
         std::string currentSection; // For handling INI sections
@@ -57,22 +99,14 @@ namespace grpc_labview {
         }
 
         configFile.close();
-        //TODO: remove this post fixing LVMessageEfficient to let enable feature. See issue: #433
-        if (featureFlags.find("data_EfficientMessageCopy") != featureFlags.end())
-            featureFlags["data_EfficientMessageCopy"] = false;
     }
 
-    // Function to check if a feature is enabled
-    bool FeatureConfig::IsFeatureEnabled(const std::string& featureName) const {
-        auto it = featureFlags.find(featureName);
-        return (it != featureFlags.end()) ? it->second : false;
-    }
+    std::filesystem::path FeatureConfig::GetDefaultConfigPath() const {
+        // Get the path of where this shared library is
+        std::filesystem::path configPath = GetFolderContainingDLL();
 
-    std::string FeatureConfig::GetDefaultConfigPath() {
-#ifdef _WIN32
-        return GetFolderContainingDLL() + "\\" + defaultConfigFileName;
-#else
-        return GetFolderContainingDLL() + "/" + defaultConfigFileName;
-#endif
+        // Add the default config filename to path
+        configPath /= defaultConfigFileName;  
+        return configPath;
     }
 }
--- src/feature_toggles.h
+++ src/feature_toggles.h
@@ -1,29 +1,10 @@
-#include <iostream>
-#include <fstream>
-#include <sstream>
+#include <string>
 #include <unordered_map>
+#include <filesystem>
+
 
 namespace grpc_labview {
     class FeatureConfig {
-    private:
-        static FeatureConfig* _instance;
-
-        std::unordered_map<std::string, bool> featureFlags;
-        const std::string defaultConfigFileName = "feature_config.ini";
-
-        // Constructor to initialize with default values
-        FeatureConfig() {
-            featureFlags["gRPC"] = true; // Enable gRPC by default as an example, this will never be overridden by config file
-            featureFlags["data_EfficientMessageCopy"] = false;
-            featureFlags["data_useOccurrence"] = true;
-
-            // Read the default configuration file
-            ReadConfigFromFile("");
-        }
-
-        // Function to get the default configuration file path
-        std::string GetDefaultConfigPath();
-
     public:
         // Singleton instance
         static FeatureConfig& getInstance() {
@@ -31,10 +12,35 @@ namespace grpc_labview {
             return instance;
         }
 
-        // Function to read feature configurations from an INI file
-        void ReadConfigFromFile(const std::string& filePath);
+        // Function to reload all features from file and re-initialize the feature map
+        void ReloadFeaturesFromFile(const std::string& filePath);
 
         // Function to check if a feature is enabled
         bool IsFeatureEnabled(const std::string& featureName) const;
+
+    private:
+        std::unordered_map<std::string, bool> featureFlags;
+        const std::string defaultConfigFileName = "feature_config.ini";
+
+        // This stores the default feature configuration.
+        // During ReloadFeaturesFromFile(), this configuration is always applied first prior to reading the
+        // features configuration file
+        void ApplyDefaultFeatures() {
+            featureFlags["gRPC"] = true; // Enable gRPC by default as an example, this will never be overridden by config file
+            featureFlags["featureFileFound"] = false;  // Used to indicate if the feature file was found/used during initialization
+            featureFlags["data_EfficientMessageCopy"] = false;
+            featureFlags["data_useOccurrence"] = true;
+        }
+        
+        FeatureConfig() {
+            // Read the default configuration file during initialization
+            ReloadFeaturesFromFile("");
+        }
+
+        // Function to get the default configuration file path
+        std::filesystem::path GetDefaultConfigPath() const;
+
+        // Reads and parses a feature configuration file
+        void ReadFeatureFile(const std::string& filePath);
     };
 }
