c2f9d77c8921698151339e74b3f4c4a0eea61bfb
Elias Faraclas <elias.faraclas@emerson.com>
2025-12-17 15:06:10 -0500

    Remove leading v when writing to file

--- build-it/update_version.py
+++ build-it/update_version.py
@@ -2,6 +2,13 @@ import argparse
 import os
 
 
+# TODO: find a better way to handle this formatting
+def format_version(input_version):
+    if input_version != "" and input_version[0] == "v":
+        input_version = input_version[1:]
+    return input_version
+
+
 def main():
     args = parse_args()
     build_script_directory = os.path.abspath(os.path.dirname(__file__))
@@ -9,7 +16,7 @@ def main():
 
     # Write the version to the VERSION file
     with open(version_file_path, "w") as version_file:
-        version_file.write(f"version={args.library_version}\n")
+        version_file.write(f"version={format_version(args.library_version)}\n")
 
 
 def parse_args():
@@ -18,7 +25,7 @@ def parse_args():
         "--library_version",
         required=True,
         default="",
-        help="New Libray Version for the VIPB files",
+        help="New Library Version to write to VERSION file",
     )
     return parser.parse_args()
 
